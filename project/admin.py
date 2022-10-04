76d29e811e0af4ac4e07adba61f1b10887cbc91b
ritishbhardwaj <ritishbhardwaj02@gmail.com>
2022-10-05 03:39:03 +0530

    #73 Re-registering the models using Decorators and readability

--- project/admin.py
+++ project/admin.py
@@ -1,29 +1,25 @@
 from django.contrib import admin
 from .models import Project, Issue, PullRequest, IssueAssignmentRequest, ActiveIssue
 
-
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'domain', 'html_url')
 
-
+@admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('title', 'number', 'project', 'mentor', 'level', 'points', 'state')
 
-
+@admin.register(PullRequest)
 class PullRequestAdmin(admin.ModelAdmin):
     list_display = ('contributor', 'pr_link', 'state', 'bonus', 'penalty', 'submitted_at')
 
-
+@admin.register(IssueAssignmentRequest)
 class IssueAssignmentRequestAdmin(admin.ModelAdmin):
     list_display = ('requester', 'issue', 'state')
 
-
+@admin.register(ActiveIssue)
 class ActiveIssueAdmin(admin.ModelAdmin):
     list_display = ('contributor', 'issue', 'assigned_at')
 
 
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Issue, IssueAdmin)
-admin.site.register(PullRequest, PullRequestAdmin)
-admin.site.register(IssueAssignmentRequest, IssueAssignmentRequestAdmin)
-admin.site.register(ActiveIssue, ActiveIssueAdmin)
+
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,10 +1,12 @@
+from faulthandler import register
 from django.contrib import admin
 from .models import UserProfile
 # Register your models here.
 
-
+@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role', 'registration_no', 'course', 'current_year', 'linkedin_id')
 
 
-admin.site.register(UserProfile, UserProfileAdmin)
+# admin.site.register(UserProfile, UserProfileAdmin)   
+# "this above line is updated as decorator in line 7 "
