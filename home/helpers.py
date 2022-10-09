b13eecdde7c414712acb1df64938857de5dd3d66
Krishna Soni <92522740+Krishna1922@users.noreply.github.com>
2022-10-09 23:10:11 +0530

    Update helpers.py

--- home/helpers.py
+++ home/helpers.py
@@ -3,6 +3,8 @@ from django.core import mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
+import threading
+from threading import Thread
 
 # email_context = {
 #     'mentor': issue.mentor,
@@ -52,3 +54,34 @@ def send_email_to_admin(template_path, email_context):
                        fail_silently=False)
     except mail.BadHeaderError:
         return mail.BadHeaderError
+    
+    
+class EmailThread(threading.Thread):
+    def __init__(self, template_path, email_context):
+        self.template_path = template_path
+        self.email_context = email_context
+        threading.Thread.__init__(self)
+
+    def run(self):
+        context = {
+            'mentor': self.email_context['mentor'].username,
+            'user': self.email_context['user'].username,
+            'url': self.email_context['url'],
+            'protocol': self.email_context['protocol'],
+            'host': self.email_context['host'],
+            'issue': self.email_context['issue'],
+            'action': self.email_context['action'],
+            'receiver': self.email_context['receiver'].username,
+        }
+
+        html_message = render_to_string(self.template_path, context=context)
+        plain_message = strip_tags(html_message)
+
+        from_email = "noreply@contriHUB-21"
+        to = str(self.email_context['receiver'].email)
+
+        try:
+            mail.send_mail(self.email_context['subject'], plain_message, from_email, [to], html_message=html_message,
+                        fail_silently=False)
+        except mail.BadHeaderError:
+            return mail.BadHeaderError
