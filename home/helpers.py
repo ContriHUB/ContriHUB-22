d6336f873218b759ac157036c2a2cc334b55a85c
Krishna Soni <92522740+Krishna1922@users.noreply.github.com>
2022-10-11 19:05:28 +0530

    Update helpers.py

--- home/helpers.py
+++ home/helpers.py
@@ -5,39 +5,6 @@ from django.utils.html import strip_tags
 import threading
 
 
-# email_context = {
-#     'mentor': issue.mentor,
-#     'user': requester,
-#     'issue_html_url': issue.html_url,
-#     'protocol': request.get_raw_uri().split('://')[0],
-#     'host': request.get_host(),
-#     'subject': "Request for Issue Assignment under ContriHUB-21.",
-# }
-# def send_email(template_path, email_context):
-#     # print(email_context)
-#     context = {
-#         'mentor': email_context['mentor'].username,
-#         'user': email_context['user'].username,
-#         'url': email_context['url'],
-#         'protocol': email_context['protocol'],
-#         'host': email_context['host'],
-#         'issue': email_context['issue'],
-#         'action': email_context['action'],
-#         'receiver': email_context['receiver'].username,
-#     }
-
-#     html_message = render_to_string(template_path, context=context)
-#     plain_message = strip_tags(html_message)
-
-#     from_email = "noreply@contriHUB-21"
-#     to = str(email_context['receiver'].email)
-
-#     try:
-#         mail.send_mail(email_context['subject'], plain_message, from_email,
-#                        [to], html_message=html_message, fail_silently=False)
-#     except mail.BadHeaderError:
-#         return mail.BadHeaderError
-
 class EmailThread(threading.Thread):
     def __init__(self, template_path, email_context):
         self.template_path = template_path
