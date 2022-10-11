b6d407556f21d72ecc91477b27d177795379749a
Krishna Soni <92522740+Krishna1922@users.noreply.github.com>
2022-10-11 13:39:53 +0530

    Update helpers.py

--- home/helpers.py
+++ home/helpers.py
@@ -13,45 +13,30 @@ import threading
 #     'host': request.get_host(),
 #     'subject': "Request for Issue Assignment under ContriHUB-21.",
 # }
-def send_email(template_path, email_context):
-    # print(email_context)
-    context = {
-        'mentor': email_context['mentor'].username,
-        'user': email_context['user'].username,
-        'url': email_context['url'],
-        'protocol': email_context['protocol'],
-        'host': email_context['host'],
-        'issue': email_context['issue'],
-        'action': email_context['action'],
-        'receiver': email_context['receiver'].username,
-    }
+# def send_email(template_path, email_context):
+#     # print(email_context)
+#     context = {
+#         'mentor': email_context['mentor'].username,
+#         'user': email_context['user'].username,
+#         'url': email_context['url'],
+#         'protocol': email_context['protocol'],
+#         'host': email_context['host'],
+#         'issue': email_context['issue'],
+#         'action': email_context['action'],
+#         'receiver': email_context['receiver'].username,
+#     }
 
-    html_message = render_to_string(template_path, context=context)
-    plain_message = strip_tags(html_message)
+#     html_message = render_to_string(template_path, context=context)
+#     plain_message = strip_tags(html_message)
 
-    from_email = "noreply@contriHUB-21"
-    to = str(email_context['receiver'].email)
+#     from_email = "noreply@contriHUB-21"
+#     to = str(email_context['receiver'].email)
 
-    try:
-        mail.send_mail(email_context['subject'], plain_message, from_email,
-                       [to], html_message=html_message, fail_silently=False)
-    except mail.BadHeaderError:
-        return mail.BadHeaderError
-
-
-def send_email_to_admin(template_path, email_context):
-    html_message = render_to_string(template_path, context=email_context)
-    plain_message = strip_tags(html_message)
-
-    from_email = "noreply@contriHUB-21"
-    to = "contrihub.avishkar@gmail.com"
-
-    try:
-        mail.send_mail(email_context['subject'], plain_message, from_email,
-                       [to], html_message=html_message, fail_silently=False)
-    except mail.BadHeaderError:
-        return mail.BadHeaderError
-
+#     try:
+#         mail.send_mail(email_context['subject'], plain_message, from_email,
+#                        [to], html_message=html_message, fail_silently=False)
+#     except mail.BadHeaderError:
+#         return mail.BadHeaderError
 
 class EmailThread(threading.Thread):
     def __init__(self, template_path, email_context):
@@ -76,6 +61,28 @@ class EmailThread(threading.Thread):
 
         from_email = "noreply@contriHUB-21"
         to = str(self.email_context['receiver'].email)
+
+        try:
+            mail.send_mail(self.email_context['subject'], plain_message,
+                           from_email, [to], html_message=html_message,
+                           fail_silently=False)
+        except mail.BadHeaderError:
+            return mail.BadHeaderError
+
+
+class EmailThread_to_admin(threading.Thread):
+    def __init__(self, template_path, email_context):
+        self.template_path = template_path
+        self.email_context = email_context
+        threading.Thread.__init__(self)
+
+    def run(self):
+        html_message = render_to_string(self.template_path,
+                                        context=self.email_context)
+        plain_message = strip_tags(html_message)
+
+        from_email = "noreply@contriHUB-21"
+        to = "contrihub.avishkar@gmail.com"
 
         try:
             mail.send_mail(self.email_context['subject'], plain_message,
